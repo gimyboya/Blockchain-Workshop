5179d013e5fb12719240f314eac77be7707df515
gimy boya <b.afatah@yahoo.com>
2017-09-16 17:12:21 +0800

    Auto stash before merge of "master" and "origin/master"

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <h1>Blockchain workshop</h1>
+    <el-input placeholder="Enter your addrees" v-model="input"></el-input>
+    <el-button type="primary" @click="getBalance">Check balance</el-button>
+    <div>Data:
+      <span>ballance: {{balance}} </span>
+    </div>
   </div>
 </template>
 
 <script>
-import nem from 'nem-sdk';
+// import nem from 'nem-sdk';
+import axios from 'axios';
 
 export default {
   data() {
     return {
-      name: 'app',
+      response: null,
+      balance: 0,
+      input: '',
     };
   },
   created() {
-    console.log(nem);
+    const that = this;
+    console.log('hello world');
+    axios.get('https://adamexperimental.cyberblox.my:7891/account/get?address=TDNDDQNA4PVCUNN2YUBG4BP2FUCGBAETB7ONION2')
+      .then((response) => {
+        console.log(response);
+        that.response = response;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  methods: {
+    getBalance() {
+      const that = this;
+      const address = this.input;
+      axios.get(`https://adamexperimental.cyberblox.my:7891/account/get?address=${address}`)
+        .then((response) => {
+          console.log(response);
+          that.balance = response.data.account.balance;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
